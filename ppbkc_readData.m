292ae71e0f1b372bfa45cc98019c199fc2f61d57
TURING\localbangertm <markbangert@gmail.com>
2018-09-28 19:50:15 +0200

    first version of base data read function

---
+++ ppbkc_readData.m
@@ -0,0 +1,70 @@
+clc
+close all
+clear
+
+%% tpr
+
+% read data from file
+fileHandle = fopen('tpr.dat');
+tprTmp = cell2mat(textscan(fileHandle,'%f %f %f %f %f %f %f %f %f %f %f %f %f %f','CommentStyle',{'#'}));
+fclose(fileHandle);
+
+tprFieldSizes = tprTmp(1,2:end);
+tprDepths     = tprTmp(2:end,1);
+
+tpr           = tprTmp(2:end,2:end);
+
+% plot
+figure
+hold on
+plot(tprDepths,tpr);
+title('tpr')
+xlabel('depth [mm]')
+ylabel('a.u.')
+grid minor
+box on
+legendStringsFieldSizes = mat2cell(tprFieldSizes',ones(13,1));
+legendStringsFieldSizes = cellfun(@num2str,legendStringsFieldSizes,'UniformOutput',false);
+legend(legendStringsFieldSizes);
+
+%% output factor
+
+% read data from file
+fileHandle = fopen('of.dat');
+outputFactor = cell2mat(textscan(fileHandle,'%f %f','CommentStyle',{'#'}));
+fclose(fileHandle);
+
+% plot
+figure
+plot(outputFactor(:,1),outputFactor(:,2))
+title('output factor')
+xlabel('field size [mm]')
+ylabel('a.u.')
+grid minor
+box on
+
+%% primary fluence
+
+% read data from file
+fileHandle = fopen('primflu.dat');
+primaryFluence = cell2mat(textscan(fileHandle,'%f %f','CommentStyle',{'#'}));
+fclose(fileHandle);
+
+% plot
+figure
+plot(primaryFluence(:,1),primaryFluence(:,2))
+title('primary fluence')
+xlabel('r [mm]')
+ylabel('a.u.')
+grid minor
+box on
+
+%% read parameters
+fileHandle = fopen('params.dat');
+tmp = textscan(fileHandle,'%s %f','CommentStyle',{'#'});
+fclose(fileHandle);
+
+params = containers.Map(tmp{1},tmp{2});
+
+%% clear up
+clear tprTmp tmp ans fileHandle;
